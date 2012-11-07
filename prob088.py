7a2c6bad6f801226bb024c103d2b218c6b19284d
Andrew Walker <walker.ab@gmail.com>
2012-11-07 23:10:13 +1100

    finished 88

--- prob088.py
+++ prob088.py
@@ -1,25 +1,25 @@
 import euler
 
-def search(m):
-    def solve(tlen, slen, minv=1):
-        if slen == 0:
-            yield []
-        else:
-            for i in xrange(minv, tlen):
-                for soln in solve(tlen, slen-1, i):
-                    yield [i] + soln
+def divisor_permutations(n, largest):
+    if n == 1:
+        yield []
+    divisors = euler.divisors(n)
+    divisors = [ d for d in divisors if d > 1 and d <= largest]
+    for d in divisors:
+        for rest in divisor_permutations(n/d, d):
+            yield [d] + rest
 
-    items = [ item for item in solve(m+1, m) ]
-    xitems = [ (sum(i),i) for i in items ]
-    xitems.sort()
-    for c, i in xitems:
-        if sum(i) == euler.product(i):
-            return i
+solns = {}
+for i in xrange(15000):
+    for divs in divisor_permutations(i, i):
+        # pad the sequence to the correct length
+        act = divs + ([1] * (euler.product(divs) - sum(divs)))
+        if len(divs) > 1:
+            if len(act) not in solns or solns[len(act)] > i:
+                solns[len(act)] = i
+vals = set()
+for k in xrange(2,12000):
+    vals.add(solns[k])
+print vals
+print 'total', sum(vals)
 
-#search(5)
-for i in xrange(2,13):
-    res = search(i)
-    try:
-        print i, res, sum(res)
-    except:
-        pass
