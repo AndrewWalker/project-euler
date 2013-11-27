33b589f23e55a0b6d8a639fb9997688a72fe8643
Andrew Walker <walker.ab@gmail.com>
2013-11-27 23:15:19 +1100

    fixed 30

--- prob030.py
+++ prob030.py
@@ -1,17 +1,18 @@
-def foo(d=4):
-    if d == 0:
-        yield []
-    else:
-        for i in range(10):
-            for rest in foo(d-1):
-                yield [i] + rest
+import euler
 
-mul = [ 10**i for i in range(6) ]
-mul.reverse()
+def numbers_with_n_digits(n):
+    return xrange(10**(n-1), 10**n)
 
-for cidx,c in enumerate(foo(6)):
-    a = sum([ mm*cc for mm,cc in zip(mul,c) ])
-    b = sum([ cc**5 for cc in c ])
-    if a == b:
-        print c,''.join([str(ci) for ci in c]),a
+def prob30():
+    cnt = 0
+    for i in xrange(2, 7):
+        for n in numbers_with_n_digits(i):
+            digits = euler.digits(n)
+            fifth_power_sum = sum([ d**5 for d in digits])
+            if fifth_power_sum == n:
+                cnt += n
+    return cnt
+
+print 'soln', cnt
+
 
