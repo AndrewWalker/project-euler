748144dafb13e9d673a839c39e97581cdecdfa0a
Andrew Walker <walker.ab@gmail.com>
2013-11-28 19:30:19 +1100

    remove dead code

--- prob005.py
+++ prob005.py
@@ -8,17 +8,6 @@ def factors(n):
 
 src = range(2,21)
 maxval = reduce( lambda a,b : a*b, src, 1)
-##src = set(src)
-##print maxval
-##print math.sqrt(maxval)
-##for i in factors(maxval):
-##    val = len(src.intersection(set(factors(i))))
-##    if val == len(src):
-##        print 'soln',i
-##        break
-##    elif val > 17:
-##        print i,val,src - src.intersection(set(factors(i)))
-###print lst
 
 x = 3*5*7*11*13*17*19
 for i in range(100):
