e20193eee0573c8d778dc8b6a4c8c8c69fedf978
Andrew Walker <walker.ab@gmail.com>
2013-12-01 20:48:14 +1100

    removed duplicates

--- prob116.py
+++ prob116.py
@@ -1,17 +1,5 @@
 from euler import *
 
-def in_mem_memoize(fun):
-    def inner(*args):
-        if not args in inner.d:
-            v = fun(*args)
-            inner.d[args] = v
-            return v
-        else:
-            return inner.d[args]
-    inner.d = {}
-    return inner
-
-
 @in_mem_memoize
 def f(x, y):
     #print 'f',x,y
