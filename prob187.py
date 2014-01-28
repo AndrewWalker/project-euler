249a9cd595c778b3111b7a193772380e6d9bf233
Andrew Walker <walker.ab@gmail.com>
2014-01-28 21:13:24 +1100

    finished 187

--- prob187.py
+++ prob187.py
@@ -1,45 +1,14 @@
-import euler
-import math
-import sys
+'''Project Euler 187
 
-def primeFactors(n):
-    if euler.isprime(n):
-        return []
-    v = n
-    i = 2
-    res = []
-    while i < int(math.sqrt(v))+1:
-        while (v % i) == 0:
-            v = v/i
-            res.append(i)
-            if len(res) > 2:
-                return []
-        i += 1
-    assert(v == 1 or euler.isprime(v))
-    if v != 1:
-        res.append(v)
-    return res
+Question. How to start?
+Answer. Research
+    Semiprimes less than 10^n
+        http://oeis.org/A066265
 
+    http://mathworld.wolfram.com/Semiprime.html
+        SemiprimesLT[x_] := Total[Map[PrimePi[x/Prime[#]] - # + 1 &, Range[1, PrimePi[Sqrt[x]]]]]
+        SemiprimesLT[10^8]
 
-def testpf():
-    c = 0
-    for i in xrange(2,10**5):
-        pf = primeFactors(i)
-        if len(pf) == 2:
-            c+=1
-        if i % 1000 == 0:
-            print i,c
-        
-
-def foo(maxv):
-    p = euler.sieve(maxv/2)
-    print p
-    c = 0
-    for i in xrange(len(p)):
-        for j in xrange(i,len(p)):
-            if p[i]*p[j] < maxv:
-                print i,j,p[i]*p[j]
-                c+=1
-    return c
-
-print foo(30)
+    Meh.
+        http://mathworld.wolfram.com/notebooks/PrimeNumbers/Semiprime.nb
+'''
