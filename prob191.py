5011e2b7fe922be19ea151477a010f8f0a80177c
Andrew Walker <walker.ab@gmail.com>
2015-01-13 22:53:20 +1100

    finished 191

--- prob191.py
+++ prob191.py
@@ -1,8 +1,15 @@
 """Project Euler 191. Prize Strings
 
+https://projecteuler.net/problem=191
+
 Question. For a day, how many outcomes are there?
 
     3 (O, L, A)
+
+    L = Late
+    A = Absent
+    O = On Time
+
 
 Question. For n days, how many outcomes are there?
 
@@ -35,47 +42,106 @@ Question. Can you use combinatorial tricks?
     b) 0 late day(s)     2^n
        in which case, there are only two choices for each of n days
 
-    n = 30
-    m = (n*2**(n-1) + 2**n)
-    d = (3**n)
-    print m
-    print d
-    print m/float(d)
+    Maybe. I couldn't work out how to do it
 """
 import timeit
 from euler import *
+from collections import defaultdict
 
-def f(duration):
+"""
+    No prize if 3 consecutive A's
+    No prize if more than 1 L
+
+"""
+
+A = 0
+L = 1
+E = 2
+O = 2
+
+INITIAL = (0,0,False)
+ERROR = (0,0,True)
+
+def transition(state, day):
+    if state == ERROR:
+        return ERROR
+    if day == L:
+        if state[L] == 0:
+            return (0, 1, False)
+        elif state[L] == 1:
+            return ERROR
+    elif day == A:
+        if state[A] == 0:
+            return (1, state[L], False)
+        elif state[A] == 1:
+            return (2, state[L], False)
+        elif state[A] == 2:
+            return ERROR
+    elif day == O:
+        return (0, state[L], False)
+    assert(1 == 0)
+
+assert transition((0,0,False), A) == (1,0,False)
+assert transition((1,0,False), A) == (2,0,False)
+assert transition((2,0,False), A) == ERROR 
+assert transition((0,1,False), A) == (1,1,False)
+assert transition((1,1,False), A) == (2,1,False)
+assert transition((2,1,False), A) == ERROR
+
+assert transition((0,0,False), L) == (0,1,False)
+assert transition((1,0,False), L) == (0,1,False)
+assert transition((2,0,False), L) == (0,1,False)
+assert transition((0,1,False), L) == ERROR
+assert transition((1,1,False), L) == ERROR
+assert transition((2,1,False), L) == ERROR
+
+assert transition((0,0,False), O) == (0,0,False)
+assert transition((1,0,False), O) == (0,0,False)
+assert transition((2,0,False), O) == (0,0,False)
+assert transition((0,1,False), O) == (0,1,False)
+assert transition((1,1,False), O) == (0,1,False)
+assert transition((2,1,False), O) == (0,1,False)
+
+def step(states):
+    out = defaultdict(int)
+    for day in [A, L, O]:
+        for s0, k0 in states.iteritems():
+            s = transition(s0, day)
+            out[s] += k0
+    return out
+
+def fast(n):
+    states = defaultdict(int)
+    states[INITIAL] = 1
+    for i in xrange(n):
+        states = step(states)
+    if ERROR in states:
+        del states[ERROR]
+    assert ERROR not in states.keys()
+    return states
+
+def last(seq):
+    d = {'O' : O, 'A' : A, 'L' : L}
+    s = INITIAL
+    for i in seq:
+        s = transition(s, d[i])
+    return s
+
+def naive(n):
+    lst = 'OAL'
     cnt = 0
-    stack = ['']
-    i = 0
-    while stack:
-        i += 1
-        if i % 10000000 == 0:
-            print (i / 3.**30)
-        cs = ['O']
-        top = stack.pop()
-        if top[-2:] != 'AA':
-            cs.append('A')
-        if 'L' not in top:
-            cs.append('L')
-        for c in cs:
-            new = str(top)
-            new += c
-            if len(new) == duration:
-                cnt += 1
-            else:
-                stack.append(new)
-    return cnt
+    out = defaultdict(int)
+    for i in itertools.product(*tuple([lst]*n)):
+        s = ''.join(i)
+        if s.count('L') > 1:
+            continue
+        if s.find('AAA') != -1:
+            continue
+        cnt += 1
+        out[last(s)] += 1
+    return out
 
-#print timeit.timeit('f(4)', setup='from __main__ import f', number=1)
-#print timeit.timeit('f(30)', setup='from __main__ import f', number=1)
-
-#for i in xrange(1,22):
-    #print f(i)
-
-ps = [ 2**i for i in xrange(10) ]
-for i in xrange(256):
-    print [ i&p for p in ps]
-
+i = 30
+d = fast(i)
+print sum(d.values())
 
