00a83b34e222f1cfdf9ca0650c4cd95ec1d8d6b2
Andrew Walker <walker.ab@gmail.com>
2015-01-14 23:05:38 +1100

    missing / fixes

---
+++ prob001.py
@@ -0,0 +1,1 @@
+print sum(i for i in xrange(1000) if i % 3 == 0 or i % 5 == 0)
--- prob003.py
+++ prob003.py
@@ -1,20 +1,3 @@
-import math
+import sympy
+print max(sympy.primefactors(600851475143))
 
-def factors(n):
-    res = set([])
-    for i in xrange(2,int(math.sqrt(n))+1):
-        if n % i == 0:
-            res.add(i)
-            res.add(n/i)
-    return res
-
-def isprime(n):
-    return len(factors(n)) == 0
-
-lst = factors(600851475143)
-lst = list(lst)
-lst.sort()
-print lst
-lst = [ n for n in lst if isprime(n)]
-print lst
-print lst[-1]
