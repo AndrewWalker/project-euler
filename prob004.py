5648f4983764a88b334e4ecda7b4971b1e795293
Andrew Walker <walker.ab@gmail.com>
2015-01-14 23:07:29 +1100

    fixes

--- prob004.py
+++ prob004.py
@@ -1,16 +1,8 @@
-def palindrome(n):
-	s = str(n)
-	for i in range(len(s)/2):
-		if s[i] != s[-(i+1)]:
-			return False
-	return True
+import euler
 
 lst = []
 for i in range(999,99,-1):
     for j in range(999,99,-1):
-        if palindrome(i*j):
+        if euler.palindrome(i*j):
             lst.append( i*j )
-    print '.',
-print lst
-lst.sort()
-print lst[-1]
+print max(lst)
