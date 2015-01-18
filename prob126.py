34a34e3b751afbdb1e840ef0187527c4ada82fe9
Andrew Walker <walker.ab@gmail.com>
2015-01-18 22:45:45 +1100

    updating 126

--- prob126.py
+++ prob126.py
@@ -1,87 +1,94 @@
-"""Project Euler 126
+"""Project Euler 126 - Cuboid Layers
+
+url: https://projecteuler.net/problem=126
+keywords: TBD
+status: started
+
+Q. What does the pattern look like?
+A. 
+
+2 x 3 x 1
+
+0: [6]                          6
+1: [6, 10, 6]                   22
+2: [6, 10, 14, 10, 6]           46  [ w*h+4*i for i in range(2*layers) ]
+
+2 x 2 x 2
+
+0:        [8]
+1:    [4, 16, 4]
+2: [4, 8, 24, 8, 4]
+
+Q. If there is a pattern, can write a function to get it
+A. Yes. It is annoying to do so however
 
 
-0, [6]                          6
-1, [6, 10, 6]                   22
-2, [6, 10, 14, 10, 6]           46  [ w*h+4*i for i in range(2*layers) ]
-4, [6, 10, 14, 18, 14, 10, 6]
-5, [6, 10, 14, 18, 22, 18, 14, 10, 6]
-
-    yyy
-   yXXXy
-  yXzzzXy
- yXz   zXy
-yXz     zXy
-yXz     zXy
- yXz   zXy
-  yXzzzXy
-   yXXXy
-    yyy
 """
-import unittest
 
-def layer_size(w, h, l, layer = 0):
-    if layer == 0:
-        return l * w * h
-    return 2*(w*h + h*l + w*l)
-    # otherwise, a new layer (in the l direction) is like
-    #
-    #if layer == 1:
-        #return
-    #if layer == 2:
-        #return layer(w, h, l, layer - 1) + 4*(l + w + h)
-    #return 0
-    #if layer == 3:
-        #return 2*(w*h + h*l + w*l) + 8*(l + w + h)
+def cuboid_layer(L,W,H, layers):
+    """How many cuboids in each layer
 
+    >>> cuboid_layer(3, 2, 1, 1)
+    22
+    >>> cuboid_layer(3, 2, 1, 2)
+    46
+    >>> cuboid_layer(3, 2, 1, 3)
+    78
+    >>> cuboid_layer(2, 2, 2, 1)
+    24
+    >>> cuboid_layer(2, 2, 2, 2)
+    48
+    >>> cuboid_layer(5, 1, 1, 1)
+    22
+    >>> cuboid_layer(5, 3, 1, 1)
+    46
+    >>> cuboid_layer(7, 2, 1, 1)
+    46
+    """
+    # top and bottom
+    c = 2*L*W 
 
-class CuboidLayersTest(unittest.TestCase):
-    def test_layer0(self):
-        self.assertEquals(layer_size(3, 2, 1, layer = 0), 6)
+    # middle 2sides of each type + 4H spacers per layer
+    c += 2*L*H + 2*W*H + 4*H*(layers-1)
 
-    def test_layer1(self):
-        self.assertEquals(layer_size(3, 2, 1, layer = 1), 22)
+    # layers that aren't the top, bottom or middle
+    for i in xrange(1, layers):
+        c += 4*(L+W+2*(i-1))
+    return c
 
-    def test_layer2(self):
-        self.assertEquals(layer_size(3, 2, 1, layer = 2), 46)
+def find_target(target = 154, N = 1000):
+    """plays the role of C(target)
 
-    def test_layer3(self):
-        self.assertEquals(layer_size(3, 2, 1, layer = 3), 78)
+    >>> find_target( 22 ) 
+    2
+    >>> find_target( 46 ) 
+    4
+    >>> find_target( 78 ) 
+    5
+    >>> find_target( 118 ) 
+    8
+    """
+    cnt = 0
+    for i in xrange(1, N):
+        for j in xrange(i, N):
+            for k in xrange(j, N):
+                if cuboid_layer(i, j, k, 1) > target:
+                    break
+                l = 1
+                while 1:
+                    size = cuboid_layer(i,j,k,l) 
+                    if size == target:
+                        cnt += 1
+                    if size >= target:
+                        break
+                    l += 1
+    return cnt
 
-    def test_layer4(self):
-        self.assertEquals(layer_size(3, 2, 1, layer = 3), 118)
-
-    def test_layer4(self):
-        self.assertEquals(layer_size(5, 1, 1, layer = 3), 22)
-
-
-#def isum(n):
-    #return n*(n+1)/2
-
-#0, [6]                          6  6+0
-#1, [6, 10, 6]                   22 18+4
-#2, [6, 10, 14, 10, 6]           46 30+16
-#4, [6, 10, 14, 18, 14, 10, 6]   78 42+36
-#5, [6, 10, 14, 18, 22, 18, 14, 10, 6]
-
-#print isum(0)*4
-#print isum(1)*4
-#print isum(2)*4 + 6
-
-#def foo(n):
-    #return (2*isum(n-1) + 1)*4+6*n
-
-#for i in xrange(4):
-    #print foo(i)
-#lsts = [
-    #[0],
-    #[0,1,0],
-    #[0,1,2,1,0],
-    #[0,1,2,3,2,1,0],
-    #[0,1,2,3,4,3,2,1,0]
-#]
-#for i, v in enumerate(lsts):
-    #t = numpy.array(v)*4+6
-    #print i, t, sum(v), numpy.sum(t)
+for i in xrange(1000):
+    print i, find_target(i)
 
 
+
+#if __name__ == "__main__":
+    #import doctest
+    #doctest.testmod()
